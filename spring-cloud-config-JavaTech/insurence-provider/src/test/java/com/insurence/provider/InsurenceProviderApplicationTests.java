11a7741760ffc083844d9ca409fb5d480e5f6162
Chetan Malabade <malabade.chetan@gmail.com>
2024-06-30 15:35:49 +0530

    config server application code commited

---
+++ spring-cloud-config-JavaTech/insurence-provider/src/test/java/com/insurence/provider/InsurenceProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurence.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsurenceProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
